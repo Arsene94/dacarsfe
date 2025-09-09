ccd06cb0803700d341fa18857f2f345e158a1b7c
Arsene94 <arsene.claudiu94@gmail.com>
2025-09-09 12:19:25 +0300

    feat(ui): add spacing to select arrow

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 import { SelectProps } from "@/types/ui";
+import { ChevronDown } from "lucide-react";
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
     (
@@ -24,24 +25,27 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
         };
 
         return (
-            <select
-                ref={ref}
-                className={cn(
-                    "w-full px-4 py-3 text-[#191919] border border-gray-300 rounded-lg focus:ring-2 focus:ring-jade focus:border-transparent font-dm-sans disabled:cursor-not-allowed disabled:opacity-50",
-                    className,
-                )}
-                onChange={handleChange}
-                value={value}
-                defaultValue={defaultValue}
-                {...props}
-            >
-                {placeholder && (
-                    <option value="" disabled hidden>
-                        {placeholder}
-                    </option>
-                )}
-                {children}
-            </select>
+            <div className="relative">
+                <select
+                    ref={ref}
+                    className={cn(
+                        "w-full appearance-none pl-4 pr-10 py-3 text-[#191919] border border-gray-300 rounded-lg focus:ring-2 focus:ring-jade focus:border-transparent font-dm-sans disabled:cursor-not-allowed disabled:opacity-50",
+                        className,
+                    )}
+                    onChange={handleChange}
+                    value={value}
+                    defaultValue={defaultValue}
+                    {...props}
+                >
+                    {placeholder && (
+                        <option value="" disabled hidden>
+                            {placeholder}
+                        </option>
+                    )}
+                    {children}
+                </select>
+                <ChevronDown className="pointer-events-none absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
+            </div>
         );
     },
 );
