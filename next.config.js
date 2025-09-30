d8b2c1501717d5cf9d79b7413d73745571915080
Arsene94 <arsene.claudiu94@gmail.com>
2025-10-01 02:50:33 +0300

    improvements

--- next.config.js
+++ next.config.js
@@ -86,7 +86,7 @@ const nextConfig = {
                 hostname: '**.dacars.ro',
             }
         ],
-        formats: ['image/avif', 'image/webp'],
+        formats: ['image/webp'],
         deviceSizes: [360, 414, 480, 640, 750, 828, 1080, 1200, 1920],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 31536000, // 1 year
