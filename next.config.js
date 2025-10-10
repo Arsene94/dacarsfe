97d43c235099c8f27c2877a38fc1984806c8b7ea
Arsene94 <arsene.claudiu94@gmail.com>
2025-10-10 11:06:35 +0300

    Treat raw HTML policy imports as source assets

--- next.config.js
+++ next.config.js
@@ -150,6 +150,12 @@ const nextConfig = {
                 'lib/polyfills/modern-runtime.ts'
             ),
         };
+
+        config.module.rules.push({
+            test: /\.html$/i,
+            resourceQuery: /raw/,
+            type: 'asset/source',
+        });
         return config;
     },
 
