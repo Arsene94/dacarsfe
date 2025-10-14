4ce633bae1af079f3f1a8ebc696327033ac3847d
Arsene94 <arsene.claudiu94@gmail.com>
2025-10-14 09:55:29 +0300

    added image quality 70

--- next.config.js
+++ next.config.js
@@ -101,7 +101,7 @@ const nextConfig = {
         formats: ['image/webp'],
         deviceSizes: [360, 414, 480, 640, 750, 828, 1080, 1200, 1920],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        qualities: [55, 60, 75, 85],
+        qualities: [55, 60, 70, 75, 85],
         minimumCacheTTL: 31536000, // 1 year
         dangerouslyAllowSVG: false,
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
