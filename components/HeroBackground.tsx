220b5033fe95e313f507838737a5b93c85cee90a
Arsene94 <arsene.claudiu94@gmail.com>
2025-10-14 09:17:05 +0300

    Optimize hero background sources

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 import heroBackgroundMobileWebp from "@/public/images/bg-hero-mobile-480x879.webp";
-import heroBackgroundMobile2xWebp from "@/public/images/bg-hero-mobile-960x1759.webp";
+import heroBackgroundMobileLargeWebp from "@/public/images/bg-hero-mobile-960x1759.webp";
 import heroBackgroundTabletWebp from "@/public/images/bg-hero-820x380.webp";
 import heroBackgroundDesktopWebp from "@/public/images/bg-hero-1280x720.webp";
+import heroBackgroundDesktopLargeWebp from "@/public/images/bg-hero-1440x810.webp";
 import heroBackgroundDesktop2xWebp from "@/public/images/bg-hero-1920x1080.webp";
 
 export const HeroBackground = () => {
@@ -12,24 +13,27 @@ export const HeroBackground = () => {
             <source
                 media="(max-width: 767px)"
                 type="image/webp"
-                srcSet={`${heroBackgroundMobileWebp.src} 1x, ${heroBackgroundMobile2xWebp.src} 2x`}
+                srcSet={`${heroBackgroundMobileWebp.src} ${heroBackgroundMobileWebp.width}w, ${heroBackgroundMobileLargeWebp.src} ${heroBackgroundMobileLargeWebp.width}w`}
+                sizes="100vw"
             />
             <source
                 media="(max-width: 1279px)"
                 type="image/webp"
-                srcSet={`${heroBackgroundTabletWebp.src} 1x, ${heroBackgroundDesktopWebp.src} 2x`}
+                srcSet={`${heroBackgroundTabletWebp.src} ${heroBackgroundTabletWebp.width}w, ${heroBackgroundDesktopWebp.src} ${heroBackgroundDesktopWebp.width}w`}
+                sizes="100vw"
             />
             <source
                 type="image/webp"
-                srcSet={`${heroBackgroundDesktopWebp.src} 1x, ${heroBackgroundDesktop2xWebp.src} 2x`}
+                srcSet={`${heroBackgroundDesktopLargeWebp.src} ${heroBackgroundDesktopLargeWebp.width}w, ${heroBackgroundDesktop2xWebp.src} ${heroBackgroundDesktop2xWebp.width}w`}
+                sizes="100vw"
             />
             <Image
-                src={heroBackgroundDesktop2xWebp}
+                src={heroBackgroundDesktopLargeWebp}
                 alt="Fundal aeroport"
                 fill
                 priority
                 placeholder="blur"
-                sizes="(max-width: 767px) 100vw, (max-width: 1279px) 100vw, 100vw"
+                sizes="(max-width: 767px) 100vw, (max-width: 1279px) 100vw, 1440px"
                 quality={70}
                 className="object-cover"
             />
