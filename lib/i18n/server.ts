fe59d1204af80f164bd4ce9b0ef8b8546aeb8959
Arsene94 <arsene.claudiu94@gmail.com>
2025-11-03 16:53:13 +0200

    Honor locale header when resolving requests

--- lib/i18n/server.ts
+++ lib/i18n/server.ts
@@ -8,8 +8,18 @@ import {
 } from "@/lib/i18n/localeDetection";
 
 const LOCALE_COOKIE_KEYS = ["dacars.locale", "NEXT_LOCALE", "locale"] as const;
+const LOCALE_HEADER_KEYS = ["x-dacars-locale"] as const;
 
 const resolveRequestLocaleUncached = async (): Promise<Locale> => {
+  const headerList = await headers();
+
+  for (const header of LOCALE_HEADER_KEYS) {
+    const locale = normalizeLocaleCandidate(headerList.get(header));
+    if (locale) {
+      return locale;
+    }
+  }
+
   const cookieStore = await cookies();
   for (const key of LOCALE_COOKIE_KEYS) {
     const locale = normalizeLocaleCandidate(cookieStore.get(key)?.value);
@@ -18,7 +28,6 @@ const resolveRequestLocaleUncached = async (): Promise<Locale> => {
     }
   }
 
-  const headerList = await headers();
   const acceptedLocales = parseAcceptLanguage(headerList.get("accept-language"));
   return pickSupportedLocale(acceptedLocales, DEFAULT_LOCALE);
 };
